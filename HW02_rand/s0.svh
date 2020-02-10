b96ba55d70abcd54b26bd679b30833c4a4179385
Jaimil <jaimilpatel30@gmail.com>
2020-02-10 01:51:05 -0800

    My test stimuli added

--- HW02_rand/s0.svh
+++ HW02_rand/s0.svh
@@ -26,13 +26,36 @@ itm mx;
      end
    endtask : donop
    
+   task dopush();
+     start_item(mx);
+     mx.opcode=E_push;
+     mx.randomize();
+     finish_item(mx);
+    endtask : dopush
+
+    task dopushcomplete();
+      start_item(mx);
+      mx.opcode=E_pushcomplete;
+      mx.randomize();
+      finish_item(mx);
+    endtask: dopushcomplete
+
 // A sequence body template. put tests there
    task body;
      mx=itm::type_id::create("seq_item");
      doreset(3);
+    `uvm_info("doreset","seq_item",UVM_MEDIUM);
      donop(3);
+     `uvm_info("donop","seq_item",UVM_MEDIUM);
 // Put your stuff here...
+    dopush;
+    #5ns;
+    `uvm_info("DO PUSH","MY TASK",UVM_MEDIUM);
+    //`uvm_info("MY",$psprintf("....",mx),UVM_MEDIUM);
+    dopushcomplete();
+    #1ns;
     
+    `uvm_do (mx);
 //
    endtask : body
 endclass : s0
