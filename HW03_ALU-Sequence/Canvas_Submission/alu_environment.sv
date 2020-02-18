ac37687bf86543b726fd973b0e47d9d6df50ddf4
Jaimil <jaimilpatel30@gmail.com>
2020-02-17 18:19:08 -0800

    Log file generated. Veersion 1

---
+++ HW03_ALU-Sequence/Canvas_Submission/alu_environment.sv
@@ -0,0 +1,24 @@
+class my_environment extends uvm_env;
+	`uvm_component_utils(my_environment)
+
+	function new (string name="my_environment", uvm_component parent=null);
+		super.new(name,parent);
+	endfunction
+	
+	//instantiate scoreboard, agent(driver, sequencer, monitor) here.
+	my_agent agnt_alu;
+	
+	//create agent and scoreboard
+	virtual function void build_phase(uvm_phase phase);
+		super.build_phase (phase);
+		agnt_alu = my_agent::type_id::create("agnt_alu",this);
+		//sb_alu = sb_alu::type_id::create ("sb_alu", this);
+	endfunction
+
+	virtual function void connect_phase(uvm_phase phase);
+		super.connect_phase(phase);
+		//
+	endfunction
+
+endclass : my_environment
+
